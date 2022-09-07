90704bd0582e2fe4ee3126031178d22be43e4830
Christopher S Nicolaysen <christopher@snicolaysen.no>
2022-09-07 08:40:50 +0200

    Added modal type +

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,11 @@ import { TestModalComponent } from './test-modal.component';
 @Component({
   selector: 'app-root',
   template: `
-    <button (click)="modal()">click me</button>
-    <pre>{{ dialogResponse$ | async | json }}</pre>
+    <div class="sticky-header">
+        <button (click)="modal()">Normal modal</button>
+        <button (click)="modalDrawer()">Drawer modal</button>
+        <pre>{{ dialogResponse$ | async | json }}</pre>
+    </div>
     <div
       style="height: 12990px; background: linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);"
     >
@@ -24,16 +27,29 @@ export class AppComponent implements OnInit {
     // this.modal();
   }
 
-  public modal() {
-    this.dialogResponse$ = this.modalService
-      .openModal(TestModalComponent, {
-        inputs: {
-          text: 'Hello world!',
-        },
-        options: {
-          title: 'With title set by opener',
-        },
-      })
-      .afterClosed();
-  }
+    public modal() {
+        this.dialogResponse$ = this.modalService
+                                   .openModal(TestModalComponent, {
+                                       inputs: {
+                                           text: 'Hello world!',
+                                       },
+                                       options: {
+                                           title: 'With title set by opener',
+                                       },
+                                   })
+                                   .afterClosed();
+    }
+    public modalDrawer() {
+        this.dialogResponse$ = this.modalService
+                                   .openModal(TestModalComponent, {
+                                       inputs: {
+                                           text: 'Hello world!',
+                                       },
+                                       options: {
+                                           title: 'With title set by opener',
+                                           type: 'drawer',
+                                       },
+                                   })
+                                   .afterClosed();
+    }
 }
